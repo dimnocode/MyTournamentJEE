567f5d07315a54f1c7fad010d8452977b19c6661
dimnocode <dimitri.krasucki@gmail.com>
2016-07-11 15:47:53 +0200

    Log4j config

--- MyTournamentV2/src/mt/servlets/TestServlet.java
+++ MyTournamentV2/src/mt/servlets/TestServlet.java
@@ -6,15 +6,16 @@ import java.util.Date;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.PersistenceException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
-import mt.entities.Game;
 import mt.entities.User;
 import mt.entities.Userrole;
 import mt.repository.UsersstatutRepository;
@@ -24,6 +25,9 @@ import mt.repository.UsersstatutRepository;
 @WebServlet("/TestServlet")
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private static final Logger logger = Logger.getLogger(TestServlet.class);
+	
     //@PersistenceUnit(unitName="MyTournamentV2")
     EntityManagerFactory emf = Persistence.createEntityManagerFactory("MyTournamentV2");
     /**
@@ -46,26 +50,31 @@ public class TestServlet extends HttpServlet {
 		//User u = new User();
 		
 		UsersstatutRepository us = new UsersstatutRepository();
+//		
+//		Userrole uss = us.find(1);
+//		uss.setName("User");
+//		//us.update(uss);
+//		em.getTransaction().begin();
+//		em.merge(uss);
+//		em.getTransaction().commit();
 		
-		Userrole uss = us.find(1);
-		uss.setName("User");
-		//us.update(uss);
-		em.getTransaction().begin();
-		em.merge(uss);
-		em.getTransaction().commit();
 		
-		/*u.setName("test");
+		User u = new User();
+		u.setName("test");
 		u.setFirstname("test");
 		u.setPseudo("t");
+		u.setPassword("pswd");
 		u.setCreationDate(new Date());
 		u.setDob(new Date());
 		u.setEmail("lol@lol.com");
 		u.setPhoneNumber("000");
-		u.setUsersstatut(us.find(1));
+		u.setUserrole(us.find(1));
 		
 		em.getTransaction().begin();
 		em.persist(u);
-		em.getTransaction().commit();*/
+		em.getTransaction().commit();
+		
+		logger.log(Level.INFO, u.getName()+ " saved");
 		
 		em.close();
 		emf.close();
