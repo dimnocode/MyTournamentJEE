b0414b51c6a997f446f820d7450bbbafce771a6f
dimnocode <dimitri.krasucki@gmail.com>
2016-07-25 14:26:50 +0200

    UserValidation

--- MyTournamentV2/src/mt/validation/UserValidation.java
+++ MyTournamentV2/src/mt/validation/UserValidation.java
@@ -12,9 +12,9 @@ public final class UserValidation {
 				!request.getParameter("nameUser").matches("^[a-zA-Z_-]{3,20}$") || 
 				!request.getParameter("firstnameUser").matches("^[a-zA-Z_-]{3,20}$") ||
 				!request.getParameter("emailUser").matches("^[^-_()!{}$&µ£=:+;,/\\.][a-zA-Z-.]+@[a-zA-Z-.]+\\.[a-zA-Z]{1,100}$")||
-				!request.getParameter("pseudoUser").matches("^[a-z0-9]+$") ||
-				!request.getParameter("phoneUser").matches("^\\d{4}///\\d{3}-\\d{3}$") ||
-				!request.getParameter("passUser").matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([A-Za-z0-9]).{3,20}$")
+				!request.getParameter("pseudoUser").matches("^[a-z0-9]{4,20}$") ||
+				!request.getParameter("phoneUser").matches("^\\d{4}/\\d{3}-\\d{3}$") ||
+				!request.getParameter("passUser").matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([A-Za-z0-9]){3,20}$")
 				
 		){
 			return false;
