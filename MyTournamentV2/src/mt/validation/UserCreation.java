932de7f35d016f622159930b8c5ccefc932969fe
dimnocode <dimitri.krasucki@gmail.com>
2016-07-25 14:15:46 +0200

    UserValidation

--- MyTournamentV2/src/mt/validation/UserCreation.java
+++ MyTournamentV2/src/mt/validation/UserCreation.java
@@ -6,8 +6,6 @@ import mt.entities.User;
 
 public final class UserCreation {
 	
-	
-	
 	public static void create(HttpServletRequest request, User user){
 		
 		
--- MyTournamentV2/src/mt/validation/UserValidation.java
+++ MyTournamentV2/src/mt/validation/UserValidation.java
@@ -2,6 +2,8 @@ package mt.validation;
 
 import javax.servlet.http.HttpServletRequest;
 
+import mt.util.Util;
+
 public final class UserValidation {
 	
 	public static boolean validate(HttpServletRequest request){
@@ -11,22 +13,24 @@ public final class UserValidation {
 				!request.getParameter("firstnameUser").matches("^[a-zA-Z_-]{3,20}$") ||
 				!request.getParameter("emailUser").matches("^[^-_()!{}$&µ£=:+;,/\\.][a-zA-Z-.]+@[a-zA-Z-.]+\\.[a-zA-Z]{1,100}$")||
 				!request.getParameter("pseudoUser").matches("^[a-z0-9]+$") ||
-				!request.getParameter("phoneUser").matches("(?=.*[a-zA-Z][0-9]).{3,20}$")
-				
+				!request.getParameter("phoneUser").matches("^\\d{4}///\\d{3}-\\d{3}$") ||
+				!request.getParameter("passUser").matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([A-Za-z0-9]).{3,20}$")
 				
 		){
 			return false;
 		}
-
 		
-
-		request.getParameter("dobUser");
-		request.getParameter("passUser");
-		request.getParameter("confirmUser");
+		if (Util.stringToDate(request.getParameter("dobUser")) == null){
+			return false;
+		}
 		
 		
+		if (!request.getParameter("passUser").equals(request.getParameter("confirmUser"))){
+			return false;
+		}
+				
 		
-		return false;
+		return true;
 		
 	}
 		
