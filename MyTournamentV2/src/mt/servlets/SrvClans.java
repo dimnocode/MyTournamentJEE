85775424a9b626d5e8d15dc7d3e2283b8a525c5b
Lucas Giunta <lucas-giunta@outlook.com>
2016-08-10 16:49:08 +0200

    clans

---
+++ MyTournamentV2/src/mt/servlets/SrvClans.java
@@ -0,0 +1,87 @@
+package mt.servlets;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import mt.connection.EMF;
+import mt.entities.Clan;
+import mt.entities.Usersclan;
+import mt.util.Util;
+import mt.validation.ClanCreation;
+import mt.validation.Validation;
+
+/**
+ * Servlet implementation class SrvClans
+ */
+@WebServlet("/clans")
+public class SrvClans extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final Logger logger = Logger.getLogger(SrvClans.class);
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SrvClans() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		if(Util.getLoggedUser(request) != null){
+			
+			this.getServletContext().getRequestDispatcher("/WEB-INF/clans.jsp").forward(request, response);
+		}else{
+			response.sendRedirect("error");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		EMF.getEMF();
+		EntityManager em = EMF.getEM();
+		
+		String btnClan = request.getParameter("btnClan");
+		String successMsg = null;
+		
+		
+		if(btnClan != null){
+			Clan clan = new Clan();
+			
+			Validation<Clan> v = new Validation<Clan>();
+			Usersclan userClan = new Usersclan();
+			if(v.validate(request, clan)){
+				ClanCreation.create(request, clan, userClan);
+				if(clan != null){
+					logger.log(Level.INFO, "Clan created :" + clan.getNom());
+					em.getTransaction().begin();
+					em.persist(clan);
+					em.persist(userClan);
+					em.getTransaction().commit();
+					successMsg = "Clan created and user is leader with success";
+					request.setAttribute("successMsg", successMsg);
+				}
+			}else{
+				response.sendRedirect("clans");
+				logger.log(Level.INFO, "Clan not valid");
+			}
+			em.close();
+		}
+		doGet(request, response);
+	}
+
+}
